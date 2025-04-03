368703054676c5be4ff12376e482336a3e5388e8
nova66-obito <sambathvj0506@gmail>
2025-04-03 14:32:36 +0530

    change3

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -15,14 +15,14 @@ export default function Nav(){
              <div className="nav-cover nav-cover1">
                  <h2 >logo</h2>
              </div>
-             <div className={`nav-cover nav-cover2 d-flex gap-4 ${open ? 'up' : 'down'}`}>
+               <div className={` nav-cover nav-cover2 d-flex gap-4 ${open ? 'up' : 'down'}`}>
                 <Link to="/">home</Link>
                 <Link to="/about">about</Link>
                 <a href="#course">course</a>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdYxFdD1a5TpMMBthUV8piowtGEUtcof5k01apWs1tuSOFX7w/viewform">admission</a>
                 <Link to="/contact">contact us</Link>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdYxFdD1a5TpMMBthUV8piowtGEUtcof5k01apWs1tuSOFX7w/viewform"><button>enroll now</button></a>
-             </div>
+                </div>
              <div className="nav-cover nav-cover3">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdYxFdD1a5TpMMBthUV8piowtGEUtcof5k01apWs1tuSOFX7w/viewform"><button>enroll now</button></a>
              </div>
